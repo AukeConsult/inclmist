dfb8854b56918ffad8da2804696f62e8063abd64
leif <leif@auke.no>
2025-02-28 12:10:26 +0100

    packages

---
+++ frontend/gpt-chat/src/app/queries/question.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ChatgptService } from '../chatgpt.service';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './question.component.html',
+  styleUrl: './question.component.css'
+})
+export class QuestionComponent {
+  userMessage = '';
+  chatHistory: { role: string, content: string, type: string }[] = [];
+
+  constructor(private chatService: ChatgptService) {}
+
+  sendMessage() {
+    this.chatHistory.push({ role: 'user', content: this.userMessage, type: 'text' });
+    this.chatService.sendMessage(this.userMessage).subscribe((response: { choices: { message: { content: any } }[] }) => {
+      // @ts-ignore
+      const botReply = response.choices[0].message.content;
+      this.chatHistory.push({ role: 'bot', content: botReply, type: 'text' });
+    });
+    this.userMessage=''
+  }
+
+  paragraphs(content: string ): string[] {
+    return content.split("\n", undefined)
+  }
+}
