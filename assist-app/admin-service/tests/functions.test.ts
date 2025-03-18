542192324863f44da5cd869193e5fe840f82a02e
leif <leif@auke.no>
2025-03-19 00:36:19 +0100

    clean up

--- assist-app/admin-service/tests/functions.test.ts
+++ assist-app/admin-service/tests/functions.test.ts
@@ -1,11 +1,3 @@
-import { credential, initializeApp } from "firebase-admin"
-import { fireBaseAdminKey } from "shared-backend/secrets";
-import { ServiceAccount } from "firebase-admin/lib/app/credential";
-
-import * as admin from "firebase-admin";
-
-const app = admin.initializeApp({credential: credential.cert(fireBaseAdminKey as ServiceAccount)})
-
 describe('function firebase', () => {
 
     it('connect to firebase', async () => {
